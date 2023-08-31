b93b0bd50645afd4dfff883d0aa227f915da32d8
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-08-31 11:02:22 +0100

    nuxt.config - added 'notranslate' specification in 'head' section

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
-      }
+      },
+      { name: 'google', content: 'notranslate' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
